9605d52bef42936e22d39515d4ea2a628ec02672
Shota-Todoroki <71693760+Shota-Todoroki@users.noreply.github.com>
2020-11-07 20:03:57 +0900

    ユーザー詳細機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "sends#index"
-  resources :sends, only: [:index, :new, :create]
+  resources :sends, only: [:index, :new, :create, :edit, :update, :show, :destroy]
+  resources :users, only: :show
 end
