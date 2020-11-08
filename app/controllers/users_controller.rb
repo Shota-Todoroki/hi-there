2ede0afe1711b80518a9eb4cd1e83b7213ce6d21
Shota-Todoroki <71693760+Shota-Todoroki@users.noreply.github.com>
2020-11-09 00:17:13 +0900

    ユーザー詳細、編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user,only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to
+    else
+      render :edit
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :self_introduction, :country_id, :gender_id, :age, :email, :password)
+  end
+  def set_user
     @user = User.find(params[:id])
   end
 end
+
